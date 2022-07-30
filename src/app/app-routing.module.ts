b38b5a7c9e5008c41539ddc100c5c4f5450e7b8e
suvarnaradhakrishnan <102644502+suvarnaradhakrishnan@users.noreply.github.com>
2022-07-30 18:54:16 +0530

    validation completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrainerEnrollComponent } from './trainer-enroll/trainer-enroll.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"enroll",component:TrainerEnrollComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/trainer-enroll/trainer-enroll.component.ts
+++ src/app/trainer-enroll/trainer-enroll.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UntypedFormGroup, UntypedFormControl, Validators, FormGroup } from '@angular/forms';
 import * as AOS from 'aos';
-import { TrainerService } from '../trainer.service';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 
 @Component({
@@ -10,26 +10,55 @@ import { TrainerService } from '../trainer.service';
   styleUrls: ['./trainer-enroll.component.css']
 })
 export class TrainerEnrollComponent implements OnInit {
+dropdownList;
+dropdownSettings;
 
-  constructor(private trainerService:TrainerService) { }
- 
+  constructor() { }
+
+ enroll_form!: FormGroup;
+
   ngOnInit(): void {
     AOS.init();
+
+    this.enroll_form=new FormGroup({
+      't_id':new UntypedFormControl(''),
+      'fname':new UntypedFormControl(''),
+      'email':new UntypedFormControl(''),
+      'phone':new UntypedFormControl(''),
+      'address':new UntypedFormControl('',Validators.required),
+      'photo':new UntypedFormControl('',Validators.required),
+      'hqual':new UntypedFormControl(''),
+      'skills':new UntypedFormControl('',Validators.required),
+      'company':new UntypedFormControl('',Validators.required),
+      'desig':new UntypedFormControl('',[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]),
+      'courses':new UntypedFormControl('',Validators.required)
+      });
+
+
+    this.dropdownList = this.getData();
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'course_id',
+      textField: 'course_name',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All'
+    };
   }
 
-    enroll_form = new FormGroup({
-    't_id':new FormControl(''),
-    'fname':new FormControl(''),
-    'email':new FormControl(''),
-    'phone':new FormControl(''),
-    'address':new FormControl('',Validators.required),
-    'photo':new FormControl('',Validators.required),
-    'hqual':new FormControl(''),
-    'skills':new FormControl('',Validators.required),
-    'company':new FormControl('',Validators.required),
-    'desig':new FormControl('',Validators.required)
-    })
-  
+  getData() : Array<any>{
+    return[
+      { course_id: 1, course_name: 'Full Stack Development'},
+      { course_id: 2, course_name: 'Software Testing'},
+      { course_id: 3, course_name: 'Data Science & Analytics'},
+      { course_id: 3, course_name: 'Robotic Process Automation'},
+      { course_id: 4, course_name: 'Cyber Security Analyst'}
+    ];
+  }
+
+  onItemSelect($event: any){
+    console.log('$event is', $event)
+  }
+
     get address(){
       return this.enroll_form.get('address');
     }
@@ -45,6 +74,9 @@ export class TrainerEnrollComponent implements OnInit {
     get desig(){
       return this.enroll_form.get('desig');
     }
+    get courses(){
+      return this.enroll_form.get('courses');
+    }
  
   onImageChangeFromFile($event:any)
   {
@@ -53,19 +85,18 @@ export class TrainerEnrollComponent implements OnInit {
       console.log(file);
       if(file.type == "image/jpeg")
       {
-        console.log("correct");
+        console.log("correct format");
       }
       else{
         this.photo?.reset();
-        alert("JPG image is required");
+        console.log("Incorrect format");
         }
     }
   }
  
   enrollSubmit(){
-    
-    this.trainerService.createProfile(this.enroll_form.value);
-    console.log("success");
+    console.log(this.enroll_form.value);
+    alert("Your information have been successfully submitted");
   }
 
 }
