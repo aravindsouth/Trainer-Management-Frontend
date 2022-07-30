0740e304ae784037198e77eab9eb7340aabb0910
RIA GEORGE <riageorge987@gmail.com>
2022-07-30 22:25:18 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,6 @@ export const routingComponents = [
   LoginComponent,
   SignupComponent,
   TrainerDashboardComponent,
-  AdminDashboardComponent
+  AdminDashboardComponent,
   TrainerListComponent
  ]
